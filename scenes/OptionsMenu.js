a6c0cdcd0d43be901652d8ca643d0f8fcd0dd111
Chad Banfield <cbanfiel@emich.edu>
2019-11-10 14:30:22 -0500

    build

--- scenes/OptionsMenu.js
+++ scenes/OptionsMenu.js
@@ -110,7 +110,7 @@ export default class OptionsMenu extends React.Component {
           </TouchableOpacity> */}
 
 
-          <TouchableOpacity style={{ width: '100%' }} onPress={() => {loadRosters(), resetFranchise(), resetSliders(), Actions.popTo('mainmenu')}}>
+          <TouchableOpacity style={{ width: '100%' }} onPress={() => {loadRosters(), resetFranchise(), resetSliders(), this.props.update(()=>Actions.popTo('mainmenu'))}}>
 
             <Card
               containerStyle={{
