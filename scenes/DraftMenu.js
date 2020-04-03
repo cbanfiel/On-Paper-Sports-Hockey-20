db40c7c6b541bf97fee47f3cb5802997f7e36d12
Chad Banfield <cbanfiel@emich.edu>
2020-04-03 16:18:29 -0400

    trade finder release

--- scenes/DraftMenu.js
+++ scenes/DraftMenu.js
@@ -129,39 +129,40 @@ export default class DraftMenu extends React.Component {
 
 
                             <View>
+                                <View style={{ display: 'flex', flexDirection: 'row', width: '95%', alignSelf: 'center' }}>
 
-                                <TouchableOpacity style={{ width: '100%' }} onPress={() => Actions.tradefinder({ popTo: Actions.currentScene, requirementsOff: true, updateScene: this.update })}>
-                                    <Card
-                                        containerStyle={{
-                                            width: '95%', backgroundColor: 'rgba(255,255,255,0)', alignSelf: 'center', borderColor: 'rgba(0,0,0,0.9)'
-                                        }}
-                                    >
+                                    <TouchableOpacity style={{ width: '97%', flex: 1, marginRight: '1.25%' }} onPress={() => Actions.tradefinder({ popTo: Actions.currentScene, requirementsOff: true, updateScene: this.update })}>
+                                        <Card
+                                            containerStyle={{
+                                                width: '100%', backgroundColor: 'rgba(255,255,255,0)', alignSelf: 'center', borderColor: 'rgba(0,0,0,0.9)'
+                                            }}
+                                        >
 
-                                        <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-                                            <CachedImage style={{ flex: 1, overflow: 'hidden', resizeMode: 'contain', height: 75, width: 75, margin: 5 }} uri={selectedTeam.logoSrc} />
-                                        </View>
-                                        <Divider style={{ backgroundColor: 'black', height: 1, margin: 5 }} ></Divider>
-                                        <Text style={{ textAlign: "center", fontSize: 20, color: 'black', fontFamily: 'advent-pro' }}>{'Trade Finder'}</Text>
-                                    </Card>
-                                </TouchableOpacity>
+                                            <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                                                <CachedImage style={{ flex: 1, overflow: 'hidden', resizeMode: 'contain', height: 75, width: 75, margin: 5 }} uri={selectedTeam.logoSrc} />
+                                            </View>
+                                            <Divider style={{ backgroundColor: 'black', height: 1, margin: 5 }} ></Divider>
+                                            <Text style={{ textAlign: "center", fontSize: 20, color: 'black', fontFamily: 'advent-pro' }}>{'Trade Finder'}</Text>
+                                        </Card>
+                                    </TouchableOpacity>
 
-                                <TouchableOpacity style={{ width: '100%' }} onPress={() => Actions.teamlist({ home: 3, back: 'season', isForced: false, updateScene: this.update, requirementsOff: true })}>
-                                    <Card
-                                        containerStyle={{
-                                            width: '95%', backgroundColor: 'rgba(0,0,0,0)',
-                                            borderColor: 'black',
-                                            alignSelf: 'center'
-                                        }}
-                                    >
+                                    <TouchableOpacity style={{ width: '97%', flex: 1, marginLeft: '1.25%' }} onPress={() => Actions.teamlist({ home: 3, back: 'season', isForced: false, updateScene: this.update, requirementsOff: true })}>
+                                        <Card
+                                            containerStyle={{
+                                                width: '100%', backgroundColor: 'rgba(0,0,0,0)',
+                                                borderColor: 'black',
+                                                alignSelf: 'center'
+                                            }}
+                                        >
 
-                                        <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-                                            <CachedImage style={{ flex: 1, overflow: 'hidden', resizeMode: 'contain', height: 75, width: 75, margin: 5 }} uri={selectedTeam.logoSrc} />
-                                        </View>
-                                        <Divider style={{ backgroundColor: 'black', height: 1, margin: 5 }} ></Divider>
-                                        <Text style={{ textAlign: "center", fontSize: 20, color: 'black', fontFamily: 'advent-pro' }}>{'Trade'}</Text>
-                                    </Card>
-                                </TouchableOpacity>
-
+                                            <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                                                <CachedImage style={{ flex: 1, overflow: 'hidden', resizeMode: 'contain', height: 75, width: 75, margin: 5 }} uri={selectedTeam.logoSrc} />
+                                            </View>
+                                            <Divider style={{ backgroundColor: 'black', height: 1, margin: 5 }} ></Divider>
+                                            <Text style={{ textAlign: "center", fontSize: 20, color: 'black', fontFamily: 'advent-pro' }}>{'Trade'}</Text>
+                                        </Card>
+                                    </TouchableOpacity>
+                                </View>
 
                                 <TouchableOpacity style={{ width: '100%' }} onPress={() => { franchise.currentDraft.simPick(), this.update() }}>
                                     <Card
